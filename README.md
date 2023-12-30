af0b714902789ee49c36fefb1e0f1f21bb451ea3
spritchard54 <>
2023-12-30 09:02:26 -0500

    updates to index.js and readme.md

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
-# OOP - SVG Logo Maker
+# SVG Logo Generator ![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)
 
+![Readme.md Screenshot](./images/rm_screenshotShape.png)
+
+## Table of Contents
+[Description](#description)<br>
+[Installation](#installation)<br>
+[Usage](#usage)<br>
+[Contributing](#contributing)<br>
+[Tests](#tests)<br>
+[License Link](#license-link)<br>
+[License Description](#license-description)<br>
+[Questions](#questions)
+
+## Description
+This application can be used to create a simple SVG Logo using command line tools. Users are presented with four prompts and the answers are used to create the logo.
+
+## Installation
+Run "node index" from terminal.
+    
+## Usage 
+This can be used for any application in your repo. Answer the questions and the logo.svg file is auto-generated. Run "node index" again to see which shape and color combinations work best for your needs. Using VS Studio, right click on the logo.svg file to view in a browser.
+    
+## Contributing 
+EDX bootcamp instructors.
+    
+## Tests
+shapes.test.js
+
+## License Link
+https://www.mit.edu/~amini/LICENSE.md
+
+## License Description
+MIT License
+
+Copyright (c) 2023 spritchard54
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+    
+## Questions
+- Github: https://github.com/spritchard54
+- Email: <swpritchard54@gmail.com>
--- index.js
+++ index.js
@@ -1,42 +1,77 @@
-const inquirer = require('inquirer');
-// const { error, clear } = require('console');
+const inquirer = require('inquirer')
+const { Triangle, Square, Circle } = require('./lib/shapes')
+const fs = require('fs')
 const questions = [
-    {
-        type: 'input',
-        name: 'userInitials',
-        message: 'Enter your initials or three letters.',
-    },
-    {
-        type: 'input',
-        name: 'whatColorText',
-        message: 'What color should your logo TEXT be? (OR a hexadecimal number)?',
-    },
-    {
-        type: 'list',
-        name: 'shapeType',
-        message: 'What type of shape do you want to use for your logo?',
-        choices: ['Cirlce', 'Square', 'Triangle']
-    },
-    {
-        type: 'input',
-        name: 'whatColorShape',
-        message: 'What color should your logo SHAPE be? (OR a hexadecimal number)?',
-    },
+  {
+    type: 'input',
+    name: 'userInitials',
+    message: 'Enter your initials or three letters.',
+    default: ''
+  },
+  {
+    type: 'input',
+    name: 'whatColorText',
+    message: 'What color should your logo TEXT be? (OR a hexadecimal number)?',
+    default: ''
+  },
+  {
+    type: 'list',
+    name: 'shapeType',
+    message: 'What type of shape do you want to use for your logo?',
+    choices: ['Circle', 'Square', 'Triangle'],
+    default: ''
+  },
+  {
+    type: 'input',
+    name: 'whatColorShape',
+    message: 'What color should your logo SHAPE be? (OR a hexadecimal number)?',
+    default: ''
+  }
 ]
+// const { error, clear } = require('console');
 
-function init() {
-    inquirer
+function createShape (_paramOne, answers) {
+  let svgLogo
+
+  if (answers.shapeType === 'Triangle') {
+    const triangle = new Triangle(answers.whatColorShape, answers.shapeType)
+    svgLogo = triangle.render()
+  } else if (answers.shapeType === 'Square') {
+    const square = new Square(answers.whatColorShape, answers.shapeType)
+    svgLogo = square.render()
+  } else {
+    const circle = new Circle(answers.whatColorShape, answers.shapeType)
+    svgLogo = circle.render()
+  }
+
+  const logoDetails = {
+    version: '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">',
+    shape: svgLogo,
+    textParams: `<text x="150" y="130" text-anchor="middle" font-size="40" fill="${answers.whatColorText}">`,
+    text: `${answers.userInitials}</text></g></svg>`
+  }
+
+  const finishedLogo = `${logoDetails.version}${logoDetails.shape}${logoDetails.textParams}${logoDetails.text}`
+
+  fs.writeFile('logo.svg', finishedLogo, (err) => {
+    if (err) {
+      throw err
+    } else {
+      console.log('The logo has been created')
+    }
+  })
+}
+
+const userQuestions = () => {
+  return inquirer
     .prompt(questions)
-        .then((answers) => {
-            // answers
-            console.log(answers);
-        });
-};
+    .then((answers) => {
+      createShape('logo.svg', answers)
+    })
+}
 
-init();
+function init () {
+  userQuestions()
+}
 
-
-// WHEN I have entered input for all the prompts THEN an SVG file is created named `logo.svg` AND the output text "Generated logo.svg" is printed in the command line
-
-// WHEN I open the `logo.svg` file in a browser
-// THEN I am shown a 300x200 pixel image that matches the criteria I entered
+init()
